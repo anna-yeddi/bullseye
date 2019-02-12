5b453a96e0ae52282f296e6988d7b55e502c9b11
Anna Kova <m3ta007@gmail.com>
2019-02-12 14:43:10 -0500

    ECMA2015 compliant, styles updated

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Sandbox to train refactoring the code with "callback hell" to cleaner one using Promises of ES6 standart.
 
+## Updates 2/12/2019:
+- JS(ES5) modernized to ECMA2015 (Promises added);
+- Copyright script and styles updated.
+
 ## Features:
 - CSS animation "Rainbow Loader" created by Avan C. and published at https://codepad.co/snippet/3SuRP9Xh;
-- HTML5, CSS3, and JS(ES5) in the original commit.
+- HTML5, CSS3, and JS(ES6).
--- script.js
+++ script.js
@@ -1,62 +1,97 @@
-let display = document.querySelector('.display'),
-    bullseye = document.querySelectorAll('.arc');
+let display = document.querySelector('#display'),
+    bullseye = document.querySelector('#rainbow'),
     body = document.querySelector('body'),
-    drink;
+    drink = 0;
 
-// bullseye.addEventListener('click', 
-    shoot({}, 
-        (mark) => {
-            display.textContent = 'You hit the target!..';
-            setTimeout(() => {
-                win(mark, buyBeer, giveMoney);
-            }, 1500);
-        },
-        (miss) => {
-            display.textContent = miss;
-            console.error(miss);
-            setTimeout(() => {
-                loose();
-            }, 1500);
-        }
-    );
-// );
+init();
 
+function init() {
+    bullseye.addEventListener('click', shootPromise);
+    start();
+}
+function shootPromise() {
+    shoot()
+        .then(win)
+        .catch(loose)
+        .then(reset)
+}
 
-function shoot(arrow, headshot, fail) {
-    display.textContent = 'The narrow is on its way';
-    body.style.cursor = 'progress';
+function start() {
+    body.removeEventListener('click', init);
+    display.style.display = 'none';
+    body.classList.add('ready');
+    console.log('init launched');
+}
 
-    setTimeout(() => {
-        Math.random() > .5 ? headshot({}) : fail('Sorry, you missed the target');
-        body.style.cursor = 'default';
-    }, 3000);
+function shoot() {
+    bullseye.removeEventListener('click', shootPromise);
+    display.style.display = 'flex';
+    display.textContent = 'The arrow is on its way...';
+    body.classList.remove('ready');
+    body.classList.add('wait');
+    console.log('shoot fired');
+
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            Math.random() > .5 ? resolve() : reject();
+            body.classList.remove('wait');
+        }, 3000);
+    });
+
+    return promise;
 }
 
 function win() {
-    display.textContent = 'You won';
-    drink = Math.random() * 1000 % 2;
-    (drink >= 1) ? buyBeer() : giveMoney();
+    display.textContent = 'You hit the target!..';
+    console.log('Lucky you!');
+    setTimeout(() => {
+        display.textContent = 'You won';
+        drink = Math.random() * 1000 % 2;
+        (drink >= 1) ? buyBeer() : giveMoney();
+        console.log('win timeout');
+    }, 1500);
 }
 
 function buyBeer() {
-    display.textContent = `${display.textContent} (and got free beer)`;
+    display.textContent = `${display.textContent} (and got an ice cream)`;
+    console.log('beer');
 }
 
 function giveMoney() {
     display.textContent = `${display.textContent} (and got some cash)`;
+    console.log('money');
 }
 
 function loose() {
-    display.textContent = 'You lost... Try again?';
+    display.textContent = 'Sorry, you missed the target!';
+    console.error('missed =((');
+    setTimeout(() => {
+        display.textContent = 'You lost... Try again?';
+        console.log('loose timeout');
+    }, 1500);
+}
+
+function reset() {
+    drink = '';
+    body.classList.remove('ready');
+    console.log('reset initiated');
+    setTimeout(() => {
+        display.textContent = 'Click to give it another shot';
+        body.addEventListener('click', init);
+        console.log('reset/timeout created!');
+    }, 1500);
+    console.log('reset finished');
 }
 
 
 // Copyright Date Script
-let dates = document.querySelector('span#date');
+let copyDates = document.querySelector('span#copyDate'),
+    pubStr = document.querySelector('span#pubDate'),
+    pubNum = parseInt(pubStr.textContent);
 
-if (new Date().getFullYear()>2019) {
-    let dateCopyright = new Date().getFullYear();
-    dates.textContent = `-${dateCopyright}`;
+if (new Date().getFullYear()>pubNum) {
+    let currDate = new Date().getFullYear();
+    copyDates.textContent = `-${currDate}`;
 }
 
 // Global site tag (gtag.js) - Google Analytics Script
